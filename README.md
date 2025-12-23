cebe8e929f6533ac6b2ed3b3ea9a24982ec0bb2d
dg189149 <104285952+dg189149@users.noreply.github.com>
2025-12-23 07:41:19 +0000

    Adds all files to the repo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CPlusPlusNotebook
+
+This program is meant to help me learn C++ a little more
---
+++ zero.s
@@ -0,0 +1,22 @@
+	.text
+	.file	"zero.cpp"
+	.globl	main                            // -- Begin function main
+	.p2align	2
+	.type	main,@function
+main:                                   // @main
+	.cfi_startproc
+// %bb.0:
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	mov	w0, wzr
+	str	wzr, [sp, #12]
+	add	sp, sp, #16
+	.cfi_def_cfa_offset 0
+	ret
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        // -- End function
+	.ident	"Ubuntu clang version 18.1.3 (1ubuntu1)"
+	.section	".note.GNU-stack","",@progbits
+	.addrsig
