9c7dbe6a4d5b71c83c53aeb6292eefc3eb81871e
dg189149 <104285952+dg189149@users.noreply.github.com>
2026-02-10 11:35:09 -0500

    Linked list lecture problems

---
+++ linkedlists/problem1.cpp
@@ -0,0 +1,22 @@
+/* The user enters integers one by one. Input ends when the user enters -9. 
+After entering -9, the user provides one more positive integer, q. 
+The program must add q to each of the previously entered integers and print the updated integers in the same order they were received. */
+
+#include <iostream>
+
+void addingIntegers(long* addressofResult) {
+    *addressofResult = 0
+    while (long q != -9) {
+        std::cin>>q;
+        *addressofResult += q
+        std::cout<<"Updated result of sum: " << *addressofResult << "\n" 
+        std::cin>>q;
+    }
+    
+}
+
+int main() {
+    long result;
+    addingIntegers(&result);
+    return 0;
+}
